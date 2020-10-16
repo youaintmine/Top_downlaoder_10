fea025f3a8c8fe7d60a4cae5d929e71dd6d13b74
Ahambarish <58632609+Ahambarish99@users.noreply.github.com>
2020-10-16 07:55:07 +0530

    Initial commit

---
+++ app/src/main/java/com/ahmbarish/top10downloaderapps/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.ahmbarish.top10downloaderapps
+
+import android.annotation.SuppressLint
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Website.URL
+import android.util.Log
+import androidx.loader.content.AsyncTaskLoader
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import kotlin.text.StringBuilder
+
+class MainActivity : AppCompatActivity() {
+            override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called")
+
+        val dataRecieved = DataFetch()
+        dataRecieved.execute("URL goes here")
+        Log.d(TAG,"onCreate : Done")
+    }
+
+    companion object {
+        private class DataFetch : AsyncTask<String, Void, String>() {
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(DATATAG, "onPost Execute : $result")
+            }
+
+            override fun doInBackground(vararg p0: String?): String {
+                Log.d(DATATAG, "doInBackground starts with : ${p0[0]}")
+                val rssFeed = downloadXML(p0[0])
+                 if(rssFeed.isEmpty()) {
+                     Log.e(DATATAG,"doInBackground : Error downloading")
+                 }
+                return rssFeed
+            }
+
+            private fun downloadXML(urlPath : String?) : String {
+                val xmlResult = StringBuilder()
+
+                try{
+                    val Url = URL(urlPath)
+                    val connect : HttpURLConnection = Url.openConnection() as HttpURLConnection
+                    val response = connect.responseCode
+                    Log.d(DATATAG,"downloadXML : The response code $response")
+
+//            val inputStream = connect.inputStream
+//            val inputStreamReader = InputStreamReader(inputStream)
+//            val reader = BufferedReader(inputStreamReader)
+                    val reader = BufferedReader(InputStreamReader(connect.inputStream))
+
+                    val inputBuffer = CharArray(500)
+                    var charRead = 0
+                    while (charRead>=0) {
+                        charRead = reader.read(inputBuffer)
+                        if (charRead>0) {
+                            xmlResult.append(String(inputBuffer,0,charRead))
+                        }
+                    }
+                    reader.close()
+
+                    Log.d(DATATAG,"Received ${xmlResult.length}")
+                    return xmlResult.toString()
+                }catch (e: MalformedURLException) {
+                    Log.e(DATATAG,"downloadXML : Invalid URL ${e.message}")
+                }catch (e:IOException) {
+                    Log.e(DATATAG,"downloadXML : IO Except ${e.message}")
+                }catch (e: SecurityException) {
+                    e.printStackTrace()
+                    Log.e(DATATAG,"Network Permission not available for network ${e.message}")
+                }catch (e : Exception) {
+                    Log.e(DATATAG,"Unknown Errors ${e.message}")
+                }
+                return ""
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/ahmbarish/top10downloaderapps/tag_log.kt
@@ -0,0 +1,4 @@
+package com.ahmbarish.top10downloaderapps
+
+val TAG = "MainActivity"
+val DATATAG = "DataFetch"
